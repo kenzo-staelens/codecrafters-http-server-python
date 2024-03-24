a37404fb878fc1569fdb9593303ed93e6344f3c9
Kenzo Staelens <kenzostael@gmail.com>
2024-03-24 09:45:36 +0100

    http 200

--- app/main.py
+++ app/main.py
@@ -1,5 +1,21 @@
 import socket
 
+class Status:
+    def __init__(self, code):
+        self.code = code
+    
+    def __repr__(self):
+        return f"HTTP/1.1 {self.code}\r\n"
+
+class HTTPResponse:
+    def __init__(self,status):
+        self.status = Status(status)
+    
+    def __repr__(self):
+        return f"{self.status}\r\n"
+
+def conn_sendall(conn, msg):
+    conn.sendall(repr(msg).encode("utf-8"))
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,7 +24,13 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221))#, reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept()
+    with conn:
+        print(f"Connected from {addr}")
+        data = conn.recv(1024)
+        if data:
+            pass
+        conn_sendall(conn, HTTPResponse(200))
 
 
 if __name__ == "__main__":
